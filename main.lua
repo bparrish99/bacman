889ef0d31db852248a559e8b11fa0a09314a53dc
Pinback <bparrish99@gmail.com>
2025-11-29 17:05:28 -0500

    I dunno

--- main.lua
+++ main.lua
@@ -90,7 +90,6 @@ function love.update(dt)
                         ghost.x = ghost.startX
                         ghost.y = ghost.startY
                         ghost.direction = "left"
-                        ghost.nextDir = ghost.direction
                         ghost.speed = Maze.getGhostSpeed(gameState.level, "norm")
                     end
                     timer.startup = 2
@@ -215,7 +214,6 @@ function love.update(dt)
                                     elseif ghost.direction == "up" then ghost.direction = "down"
                                     else ghost.direction = "up"
                                     end
-                                    ghost.nextDir = ghost.direction
                                 end
                             end
                         end
